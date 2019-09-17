6d127f94325de6995a3fb1bde38617ca3fc54812
zxw <1012596161@qq.com>
2019-09-17 17:49:44 +0800

    再次置空

--- src/components/caseDetails/involvedCase.vue
+++ src/components/caseDetails/involvedCase.vue
@@ -78,6 +78,8 @@ export default {
     },
     initData(caseid) {
       console.log("this.graphteam0:", this.graphteam, "this.nodesteam0", this.nodesteam);
+      this.graphteam = null;
+      this.nodesteam = null;
       this.$axios
         .post("http://50.64.129.46:8030/findTeamAndPersonByRecord", { recordId: caseid })
         // this.$axios({
